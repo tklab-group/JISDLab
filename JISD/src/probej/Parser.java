b43e3279447db750f61488ffadf948886a025281
sakupo <sakuposaku@icloud.com>
2020-11-19 16:37:33 +0900

    Fix a bug related to parsing Location data when a method name is empty.

--- JISD/src/probej/Parser.java
+++ JISD/src/probej/Parser.java
@@ -19,20 +19,25 @@ class Parser {
   
   Optional<Location> parseLocation(String locStr) {
     String[] locs = locStr.split(",", 0);
-    if (locs.length != 4) {
+    if (locs.length != 3 && locs.length != 4) {
       return Optional.empty();
     }
+    /*if locs.length == 3, methodName is empty*/
+    int offset = locs.length-4;
     String className  = SrcUtil.toClassNameFromSourcePath(locs[0]);
-    String methodName = locs[1];
-    int lineNumber    = Integer.parseInt(locs[2]);
-    String varName    = locs[3];
+    String methodName = "";
+    if (locs.length == 4) {
+      methodName = locs[1];
+    }
+    int lineNumber    = Integer.parseInt(locs[2+offset]);
+    String varName    = locs[3+offset];
     var loc = new Location(className, methodName, lineNumber, varName);
     return Optional.ofNullable(loc);
   }
   
   Optional<ValueInfo> parseValue(String valueStr) {
     String[] values = valueStr.split(",", 0);
-    if (values.length != 8) {
+    if (values.length != 7 && values.length != 8) {
       return Optional.empty();
     }
     var value = new PrimitiveInfo(values[0]);
