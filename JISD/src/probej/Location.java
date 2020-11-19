c46d25c1e5932e72afbe569bce8ec63a32619681
sakupo <sakuposaku@icloud.com>
2020-11-19 17:17:14 +0900

    Add getters in Location.java.

--- JISD/src/probej/Location.java
+++ JISD/src/probej/Location.java
@@ -18,4 +18,28 @@ public class Location {
     this.lineNumber = lineNumber;
     this.varName = varName;
   }
+  /**
+   * @return the className
+   */
+  public String getClassName() {
+    return className;
+  }
+  /**
+   * @return the methodName
+   */
+  public String getMethodName() {
+    return methodName;
+  }
+  /**
+   * @return the lineNumber
+   */
+  public int getLineNumber() {
+    return lineNumber;
+  }
+  /**
+   * @return the varName
+   */
+  public String getVarName() {
+    return varName;
+  }
 }
