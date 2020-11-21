dca1a4f4b2145f916e935e53b4120f6250ba21fe
sakupo <sakuposaku@icloud.com>
2020-11-22 03:12:39 +0900

    Add test for Static Class and its child Classes.

--- JISD/test/src/_static/StaticTest.java
+++ JISD/test/src/_static/StaticTest.java
@@ -18,8 +18,22 @@ public class StaticTest {
   @Test
   public void jsonFileReadTest() {
     var s = new StaticFile();
-    Assertions.assertNotNull(s.getMd());
+    Assertions.assertNotNull(s.getCd());
     Assertions.assertNotNull(s.getPs());
     out(s.getPs().toString());
+  }
+
+  @Test
+  public void getStaticTest() {
+    var s = new StaticFile();
+    Class c = new Class("src", "demo.HelloWorld");
+    out(c.fields());
+    out(c.methods());
+    var f = c.field("helloTo");
+    f.name();
+    var m = c.method("main(java.lang.String[])");
+    out(m.locals());
+    var l = m.local("s");
+    l.name();
   }
 }
