d8780900cba3897fbb0832a602001ac11971b795
sakupo <sakuposaku@icloud.com>
2020-11-22 03:09:29 +0900

    Add locals method.

--- JISD/src/_static/Method.java
+++ JISD/src/_static/Method.java
@@ -1,8 +1,16 @@
 package _static;
 
+import org.json.JSONException;
+import util.Name;
+import util.Print;
+import util.Stream;
+
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class Method extends Static {
+  private Optional<ArrayList<String>> locals = Optional.empty();
+
   public Method(String srcDir, String className, String methodName) {
     super(srcDir, className, methodName);
   }
@@ -12,6 +20,25 @@ public class Method extends Static {
   }
 
   public ArrayList<String> locals() {
-    return null;
+    if (locals.isPresent()) {
+      return locals.get();
+    }
+    var ps = StaticFile.getPs();
+    var packageAndClassName = Name.splitClassName(className);
+    if (ps.isEmpty()) {
+      return new ArrayList<>();
+    }
+    var psObj = ps.get();
+    try {
+      var packageObj = psObj.getJSONObject(packageAndClassName.get("package"));
+      var classObj = packageObj.getJSONObject(packageAndClassName.get("class"));
+      var methodsObj = classObj.getJSONObject(name);
+      var names = (ArrayList<String>) methodsObj.keySet().stream().collect(Stream.toArrayList());
+      locals = Optional.of(names);
+      return names;
+    } catch (JSONException e) {
+      Print.out("Data not found");
+      return new ArrayList<>();
+    }
   }
 }
