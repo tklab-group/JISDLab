b8107b580ac4c2c8e7fc3fe8f265f1f256f9dd21
sakupo <sakuposaku@icloud.com>
2020-11-23 02:35:58 +0900

    Add Json utility.

---
+++ JISD/src/util/Json.java
@@ -0,0 +1,42 @@
+package util;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Optional;
+
+public class Json {
+  public static Optional<JSONObject> readJsonFile(String jsonFilePath) {
+    Path path = Paths.get(jsonFilePath);
+    String jsonStr;
+    try {
+      jsonStr = Files.readString(path);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return Optional.empty();
+    }
+    JSONObject jsonObj = new JSONObject(jsonStr);
+    return Optional.ofNullable(jsonObj);
+  }
+
+  public static void writeJsonFile(String jsonFilePath, JSONObject jsonObj) {
+    Path path = Paths.get(jsonFilePath);
+    try {
+      Files.writeString(
+          path,
+          jsonObj.toString(),
+          Charset.forName("UTF-8"),
+          StandardOpenOption.WRITE,
+          StandardOpenOption.CREATE,
+          StandardOpenOption.TRUNCATE_EXISTING);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return;
+    }
+  }
+}
