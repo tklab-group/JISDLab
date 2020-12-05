dbab29f2339e470eb9ef89a4d94690faa660d8ad
sakupo <sakuposaku@icloud.com>
2020-12-05 20:30:57 +0900

    Fix bindir name bug.

--- JISD/src/_static/StaticFile.java
+++ JISD/src/_static/StaticFile.java
@@ -15,7 +15,8 @@ public class StaticFile {
   @Setter static String rootDirPath = rootDirName + File.separator;
   @Setter static String classDataFilePath = rootDirPath + "class_data.json";
   @Setter static String programStructureFilePath = rootDirPath + "program_structure.json";
-  @Getter @Setter static String srcDir = ".", binDir = ".";
+  @Getter @Setter static String srcDir = ".";
+  @Getter @Setter static String binDir = ".";
   @Getter static Optional<JSONObject> cd = Optional.empty();
   @Getter static Optional<JSONObject> ps = Optional.empty();
   // private static StaticFile me = new StaticFile();
@@ -24,26 +25,26 @@ public class StaticFile {
 
   public static void load(String srcDir, String binDir) {
     if (!srcDir.isBlank()) {
-      setSrcDir(srcDir);
+      setSrcDir((srcDir.endsWith(File.separator)) ? srcDir : srcDir + File.separator);
     }
     if (!binDir.isBlank()) {
-      setBinDir(binDir);
+      setBinDir((binDir.endsWith(File.separator)) ? binDir : binDir + File.separator);
     }
-    setStaticFilePath(binDir);
+    setStaticFilePath();
     createStaticData();
     readStaticData();
   }
 
-  static void setStaticFilePath(String binDir) {
-    setRootDirPath(binDir + File.separator + rootDirName + File.separator);
+  static void setStaticFilePath() {
+    setRootDirPath(binDir + rootDirName + File.separator);
     setClassDataFilePath(rootDirPath + "class_data.json");
     setProgramStructureFilePath(rootDirPath + "program_structure.json");
   }
 
   static void createStaticData() {
     String[] args = new String[2];
-    args[0] = rootDirPath;
-    args[1] = binDir;
+    args[0] = rootDirPath; // out
+    args[1] = binDir; // in
     LysMain.main(args);
   }
 
