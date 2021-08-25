39e7bf375cfec1bef5c8a17d2846f3620d7f2a4c
sakupo <sakuposaku@icloud.com>
2021-08-26 04:10:33 +0900

    Fix a bug when changing lang

--- docs/assets/js/sidebar.js
+++ docs/assets/js/sidebar.js
@@ -1,4 +1,4 @@
-const defaultLang = "en";
+let currentLang = "en";
 const text = {
   installation:
     {ja: ["インストール"],
@@ -45,8 +45,8 @@ function setLang(e, from, to) {
 }
 
 function createPage() {
-  const lang = sessionStorage.getItem('lang') ?? defaultLang;
-  if (lang === defaultLang) return;
+  const lang = sessionStorage.getItem('lang') ?? currentLang;
+  if (lang === currentLang) return;
   const site_title_e = getFirstElement("site-title");
   const installation_e = getFirstElement("installation");
   const getting_started_e = getFirstElement("getting-started");
@@ -59,22 +59,23 @@ function createPage() {
     default:
       return;
   }
-  setLang(site_title_e, defaultLang, lang);
-  setLang(installation_e, defaultLang, lang);
+  setLang(site_title_e, currentLang, lang);
+  setLang(installation_e, currentLang, lang);
   installation_e.text = getText("installation", lang, 0);
-  setLang(getting_started_e, defaultLang, lang);
+  setLang(getting_started_e, currentLang, lang);
   getting_started_e.text = getText("getting_started", lang, 0);
   for (let index = 0; index < getting_started_items_e.length; index++) {
     const item_e = getting_started_items_e[index].firstElementChild;
-    setLang(item_e, defaultLang, lang);
+    setLang(item_e, currentLang, lang);
     item_e.text = getText("getting_started", lang, index+1);
   }
+  currentLang = lang;
 }
 
 function changeLang(lang) {
   const site_title_e = getFirstElement("site-title");
-  const home_url = setLang(site_title_e, defaultLang, lang);
-  const next_location = replaceLangInString(location.href, defaultLang, lang);
+  const home_url = setLang(site_title_e, currentLang, lang);
+  const next_location = replaceLangInString(location.href, currentLang, lang);
   sessionStorage["lang"] = lang;
   location.href = (next_location === location.href) ? home_url : next_location;
 }
